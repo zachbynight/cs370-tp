009de6221138be931db7078bc0a39c7501f5f6ae
Zach Townsend <zachtown@colostate.edu>
2026-05-07 17:05:47 -0600

    still working

--- Client.py
+++ Client.py
@@ -5,30 +5,26 @@ import sys
 
 
 SERVER_IP = ""
-PORT = 5000
+PORT = 5002
+
+
+EXIT_INSTRUCTION = "exit"
 
 
 def main():
     SERVER_IP = sys.argv[1]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((SERVER_IP, PORT))
-    if s.recv(1) != b'1':
-        print("Connected could not be started")
-        return
     print("Connected")
     while True:
-        input_str = input()
-        input_type = determine_input_type(input_str)
-        if input_type == INPUT_PASS:
-            print("Invalid input")
-            continue
-        if input_type == INPUT_EXIT:
-            s.sendall("exit".encode())
-            print("Closing connection")
-            s.close()
+        print(f"[{time.time()}] check if server closed")
+        if s.recv(1024).decode == EXIT_INSTRUCTION:
+            print(f"[{time.time()}] Server has closed")
+        cont = handle_input(s, input())
+        if not cont:
             break
-        new_instruction = Instruction.parse(input_str)
-        send_instruction(s, str(new_instruction))
+    print(f"[{time.time()}] Closing connection")
+    s.close()
     print("Exiting program")
 
 
@@ -36,9 +32,20 @@ INPUT_PASS = 0
 INPUT_EXIT = 1
 INPUT_INSTRUCTION = 2
 
+def handle_input(s, input_str):
+    input_type = determine_input_type(input_str)
+    if input_type == INPUT_PASS:
+        return True
+    if input_type == INPUT_EXIT:
+        send_instruction(s, EXIT_INSTRUCTION)
+        return False
+    new_instruction = Instruction.parse(input_str)
+    send_instruction(s, str(new_instruction))
+    return True
+
 def determine_input_type(input_str):
     input_str = input_str.strip()
-    if input_str == "exit":
+    if input_str == EXIT_INSTRUCTION:
         return INPUT_EXIT
     if Instruction.is_parseable(input_str):
         return INPUT_INSTRUCTION
@@ -46,14 +53,9 @@ def determine_input_type(input_str):
 
 def send_instruction(s, message_string):
     try:
-        # print("check if server closed")
-        if s.recv(1024).decode == "exit":
-            s.close()
-            # print("connected closed")
-            return
-        # print("try sending")
+        print(f"[{time.time()}] try sending")
         s.sendall(message_string.encode())
-        # print(f"Sent {message_string}")
+        print(f"[{time.time()}] Sent {message_string}")
     except Exception as e:
         print(f"ERROR: {e}")
 
@@ -82,13 +84,18 @@ class Instruction:
         self.good_message = good_message
         self.bad_message = bad_message
     
-    def __str__(self):
-        text = {
-            0: self.expression,
-            1: self.good_message,
-            2: self.bad_message
+    def as_json(self):
+        return {
+            "instruction": 
+                [
+                    self.expression,
+                    self.good_message,
+                    self.bad_message
+                ]
         }
-        return json.dumps(text)
+    
+    def __str__(self):
+        return json.dumps(self.as_json())
         
         
 main()
--- Server.py
+++ Server.py
@@ -2,12 +2,14 @@ import time
 import socket
 import board
 import DHT
+import json
 from Events import Event
 import threading
 
 
 HOST = '0.0.0.0'
-PORT = 5000
+PORT = 5002
+EXIT_INSTRUCTION = "exit"
 
 
 events = []
@@ -27,55 +29,66 @@ def main():
 
 def process_network_requests():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(f"Starting server @ {socket.gethostname()}")
+    print(f"[{time.time()}] Starting server @ {socket.gethostname()}")
     s.bind((HOST, PORT))
     s.listen()
-    print(f"Server listening on {PORT}...")
+    print(f"[{time.time()}] Server listening on {PORT}...")
     while True:
-        if input() == "exit":
-            break
+        print(f"{time.time()} Checking for new connection...")
         connected_socket, address = s.accept()
         try:
             handle_connection(connected_socket, address)
         except Exception as e:
-            print(f"Error occurred while connected to {address}: {e}")
+            print(f"[{time.time()}] Error occurred while connected to {address}, closing connection.\n{e}")
+            connected_socket.close()
     s.close()
-    print("Server closing")
+    print(f"{time.time()} Server closing")
 
 def handle_connection(connected_socket, address):
-    print(f"Connected to {address}")
+    print(f"{time.time()} Connected to {address}")
     while True:
+        #print(f"[{time.time()}] initiate conversation")
         connected_socket.send(b'1')
-        decoded = handle_message(connected_socket, address)
-        if decoded == "exit":
+        decoded = decode_message(connected_socket, address)
+        if decoded == "exit" or decoded == "":
             break
-        new_event = parse_message(decoded)
-        if new_event == None:
-            continue
-        events.append(new_event)
+        #print(f"[{time.time()}] handle message")
+        handle_message(decoded)
+    print(f"[{time.time()}] Connection with {address} has ended")
     connected_socket.close()
 
-def handle_message(connected_socket, address):
+def decode_message(connected_socket, address):
+    print(f"[{time.time()}] decode message")
     data = connected_socket.recv(1024)
     if not data:
         return ""
     decoded = data.decode()
-    print(f"Received Instruction Data: {decoded}")
+    print(f"[{time.time()}] Received Instruction Data: {decoded}")
     return decoded
 
-def parse_message(message):
-    items = message.split()
-    if len(items) != 2 and len(items) != 3:
-        return False
-    if len(items) == 2:
-        items.append("")
-    return Event(items[0], items[1], items[2])
+def handle_message(decoded_message):
+    print(f"[{time.time()}] handle message")
+    new_event = None 
+    try:
+        new_event = parse_message(decoded_message)
+    except:
+        print("Error parsing message")
+        return
+    if new_event == False:
+        return
+    print("new event")
+    events.append(new_event)
+
+def parse_message(decoded_message):
+    print(f"[{time.time()}] parse message")
+    decoded_message = json.loads(decoded_message)["instruction"]
+    return Event(decoded_message[0], decoded_message[1], decoded_message[2])
 
 
 def process_sensor():
     print("Starting sensor")
     sensor = DHT.DHT(board.D27)
-    events.append(Event("True", "%r"))
+    #events.append(Event("True", "%r"))
     while True:
         time.sleep(1)
         reading = sensor.read()
